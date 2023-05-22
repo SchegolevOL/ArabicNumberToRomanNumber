575cff643f28e9247e1ea99939818789cf0e2498
Oleg Schegol <olschegolev@yahoo.com>
2023-05-22 14:26:13 +0500

    Add project files.

---
+++ ArabicNumberToRomanNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] Arabik = {1000, 500, 100, 50, 10, 5, 1};
+char[] Roman = { 'M', 'D','C', 'L', 'X', 'V', 'I'};
+Console.WriteLine("Enter to number : ");
+int arabicNumber = Convert.ToInt32(Console.ReadLine());
+string result = "";
+for (int i = 0; i < Roman.Length; i++)
+{
+	while (arabicNumber >= Arabik[i])
+	{
+		arabicNumber -= Arabik[i];
+		result += Roman[i];
+	}
+}
+Console.WriteLine(result);
